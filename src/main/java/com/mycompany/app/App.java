f5fc7ebac0b193ae25622ff76caa4ca9a08feda2
Mekontso Anderson <mekontsoanderson@gmail.com>
2025-06-07 14:52:28 +0100

    Downgrade maven

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,8 +1,5 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
 public class App {
 
     private static final String MESSAGE = "Hello World!";
@@ -10,7 +7,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE)
+        System.out.println(MESSAGE);
     }
 
     public String getMessage() {
